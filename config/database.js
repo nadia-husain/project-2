6a2beadbe81cf68aa339258ea7eb48e8c312bf39
Zahid Allaulddin <zahidalaauddin@gmail.com>
2023-02-25 16:45:37 +0300

    fix database link

--- config/database.js
+++ config/database.js
@@ -1,5 +1,12 @@
 const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+const path = require('path')
+
+dotenv.config({path: '.env'})
+
 const dbURI = process.env.DATABASE_URI
+
+mongoose.set('strictQuery', true)
 
 mongoose.connect(dbURI, {
     useNewUrlParser: true,
