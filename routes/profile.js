256ccb6ff2c809f8b02bca4797d7897604d2c9e0
Zahid Allaulddin <zahidalaauddin@gmail.com>
2023-02-27 13:57:09 +0300

    redirect profile to sign in when not signed in

--- routes/profile.js
+++ routes/profile.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router()
+const isLoggedIn = require('../lib/isLoggedIn')
 
 const profileController = require('../controllers/profile')
 
-router.get('/profile/index', profileController.profile_index_get)
+router.get('/profile/index', isLoggedIn, profileController.profile_index_get)
 
 module.exports = router
