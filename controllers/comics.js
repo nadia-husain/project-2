90af24b3073941af94835bb9177879713f60ec95
Zahid Allaulddin <zahidalaauddin@gmail.com>
2023-02-27 12:14:29 +0300

    change some links to buttons

--- controllers/comics.js
+++ controllers/comics.js
@@ -1,4 +1,4 @@
-// const Comic = require('../models/Comic')
+const Comic = require('../models/Comic')
 
 exports.comic_index_get = function (req, res) {
     res.render('comic/index')
@@ -11,4 +11,13 @@ exports.comic_create_get = function (req, res) {
 exports.comic_create_post = function (req, res) {
      console.log(req.body)
      let comic = new Comic(req.body)
+
+     comic.Save()
+     .then(function (){
+        res.redirect('/comic/index')
+    .catch(function (err){
+        console.log(err)
+        res.send('An error occured please try again later')
+    })
+     })
 }
